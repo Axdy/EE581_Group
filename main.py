41f69a3e3b4de478405726118b8e8b5e67be1104
Ian Bowyer <iancbowyer@gmail.com>
2018-03-03 20:54:43 +0000

    train against random data

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from torch.autograd import Variable
 import torch.nn as nn
 import torch.nn.functional as F
 import torchvision as tv
+import torch.optim as optim
 
 class Net(nn.Module):
     def __init__(self):
@@ -25,11 +26,41 @@ path = './BSR_bsds500/BSR/BSDS500/data/images/train'
 BSR_transform = tv.transforms.Compose([
     tv.transforms.Grayscale(),
     tv.transforms.Lambda(lambda x : imrotate(x)),
-    tv.transforms.ToTensor()
+    tv.transforms.Resize((48,32)),
+    tv.transforms.ToTensor(),
+    tv.transforms.Normalize((0.5,),(0.5,))
 ])
 
 train_set = tv.datasets.ImageFolder(root = path, transform = BSR_transform)
-train_loader = torch.utils.data.DataLoader(train_set, batch_size=20, shuffle=True)
 
-for data in train_loader:
-    print(data[0].size())
+
+train_gt_set = []
+
+#load ground truths and add to train set
+for data in train_set:
+    test = torch.randn(1,44,28)
+    train_gt_set.append((data[0],test))
+
+train_loader = torch.utils.data.DataLoader(train_gt_set, batch_size=40, shuffle=True)
+
+net = Net()
+
+criterion = nn.MSELoss()
+optimiser = optim.SGD(net.parameters(), lr=0.01)
+num_epochs = 5
+
+for epochs in range(num_epochs):
+    for data in train_loader:
+        inputs, segments = data
+
+        inputs, segments = Variable(inputs), Variable(segments)
+
+        optimiser.zero_grad()
+
+        outputs = net(inputs)
+
+        loss = criterion(outputs, segments)
+
+        loss.backward()
+        optimiser.step()
+
